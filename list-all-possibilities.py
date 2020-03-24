12398cf7e6af335d6fecc8175b4134d03ee4aac7
Waterloo Dark <60177266+waterloodark@users.noreply.github.com>
2020-03-24 12:59:20 +0900

    Update list-all-possibilities.py

--- list-all-possibilities.py
+++ list-all-possibilities.py
@@ -1,5 +1,7 @@
 import datetime as dt
-import matplotlib.pyplot as plt
+import csv
+import sys
+#import matplotlib.pyplot as plt
 
 
 class SlotMachine:
@@ -13,36 +15,51 @@ class SlotMachine:
         payout_win, payout_bb, payout_rb = payout
         p_lottery = [pr_lose, pr_win, pr_replay]
         if r_bb > 0:
-            p_lottery = p_lottery + [pr_bb] + [0] * (r_bb -1)
+            p_lottery = p_lottery + [pr_bb] + [0] * (r_bb)
         if r_rb > 0:
-            p_lottery = p_lottery + [pr_rb] + [0] * (r_rb -1)
+            p_lottery = p_lottery + [pr_rb] + [0] * (r_rb)
         self.P = []
         self.P = [p_lottery, p_lottery, p_lottery]
         if r_bb > 0:
             p_bb = []
-            for i in range(r_bb - 1):
-                p_bb.append([0] * 3 + \
-                            [0] * (i + 1) + [1] + [0] * (r_bb - i - 2) + \
-                            [0] * r_rb)
+            for i in range(r_bb):
+                if r_rb == 0:
+                    p_bb.append([0] * 3 + \
+                                [0] * (i + 1) + [1] + \
+                                [0] * (r_bb - i - 1))
+                else:
+                    p_bb.append([0] * 3 + \
+                                [0] * (i + 1) + [1] + \
+                                [0] * (r_bb - i - 1) + \
+                                [0] + [0] * r_rb)
             self.P = self.P + p_bb + [p_lottery]
         if r_rb > 0:
             p_rb = []
-            for i in range(r_rb - 1):
-                p_rb.append([0] * 3 + \
-                            [0] * r_bb + \
-                            [0] * (i + 1) + \
-                            [1] + [0] * (r_rb - i - 2))
+            for i in range(r_rb):
+                if r_bb == 0:
+                    p_rb.append([0] * 3 + \
+                                [0] * (i + 1) + \
+                                [1] + [0] * (r_rb - i - 1))
+                else:
+                    p_rb.append([0] * 3 + \
+                                [0] + [0] * r_bb + \
+                                [0] * (i + 1) + \
+                                [1] + [0] * (r_rb - i - 1))
             self.P = self.P + p_rb + [p_lottery]
 
         # 状態数
-        self.n_state = 1 + 1 + 1 + r_bb + r_rb
+        self.n_state = 1 + 1 + 1 + 1 + r_bb + 1 + r_rb
 
         # 払い出しメダル数
-        self.payout = [0] * 3 + [payout_bb] * r_bb + [payout_rb] * r_rb
+        self.payout = [0, payout_win, 0]
+        if r_bb > 0:
+            self.payout = self.payout + [0] + [payout_bb] * r_bb
+        if r_rb > 0:
+            self.payout = self.payout + [0] + [payout_rb] * r_rb
 
         # 投入メダル数
         self.slotin = [slotin_lottery] * 2 + [0] + \
-                      [slotin_bb] * r_bb + [slotin_rb] * r_rb
+                      [slotin_bb] * (r_bb + 1) + [slotin_rb] * (r_rb + 1)
 
     def configure(self, model):
         if model == 1:
@@ -74,7 +91,6 @@ class SlotMachine:
             r_rb = 8  # RB繰り返し数
             slotin_rb = 1  # メダル投入数 (RB)
             payout_rb = 15  # メダル払い出し数 (RB)
-
         elif model == 3:
             pr_replay = 1.0/7.3  # 再遊技発生確率
             pr_win = 0.1724  # 入賞確率
@@ -98,18 +114,16 @@ class SlotMachine:
 
     def initialize(self):
         self.states = {(0, 0, 0): 1}
-        self.epoch = [[dt.datetime.now(), len(self.states)]]
 
     def update(self):
         states_new = {}
         for s in list(self.states.keys()):
-            p_trans = self.P[s[0]]
+            p_trans = self.P[s[1]]
             pr = self.states[s]
-            slot_in = self.slotin[s[0]]
-            s_trans = []
+            slot_in = self.slotin[s[1]]
             for k in range(len(p_trans)):
                 if p_trans[k]>0:
-                    s_new = (k, s[1] + slot_in, s[2] + self.payout[k])
+                    s_new = (s[0] + slot_in, k, s[2] + self.payout[k])
                     pp = p_trans[k]
                     if s_new in states_new.keys():
                         states_new[s_new] += pr * pp
@@ -117,31 +131,85 @@ class SlotMachine:
                         states_new[s_new] = pr * pp
                 
         self.states = states_new
-        self.epoch.append([dt.datetime.now(), len(self.states)])
+
 timestamp_begin = dt.datetime.now()
 m = SlotMachine()
-m.configure(1)
-#for i in range(len(m.P)):
-#    print(m.P[i])
-#print([len(m.P), [len(m.P[i]) for i in range(len(m.P))]])
-#print(m.slotin)
-#print(m.payout)
+if sys.argv[1] == '1':
+    m.configure(1)
+elif sys.argv[1] == '2':
+    m.configure(2)
+else:
+    m.configure(3)
+for i in range(len(m.P)):
+    print(m.P[i])
+print([len(m.P), [len(m.P[i]) for i in range(len(m.P))]])
+print(m.slotin)
+print(m.payout)
 m.initialize()
+timestamp_begin = dt.datetime.now()
+epoch = [[0,0]]
+print([dt.datetime.now(), 0, 0])
 for i in range(1600):
     m.update()
-print(len(m.states))
-print(sum(m.states.values()))
-timestamp_end = dt.datetime.now()
-print(timestamp_end - timestamp_begin)
+    # epoch.append([(dt.datetime.now()-timestamp_begin).total_seconds(), len(m.states)])
+    if (i + 1) % 10 == 0:
+        print([dt.datetime.now(), i + 1, len(m.states)])
+    if i + 1 == 400:
+        print(len(m.states))
+        print(sum(m.states.values()))
+        timestamp_end = dt.datetime.now()
+        print(timestamp_end - timestamp_begin)
 
-slotin_mean = sum([key[1]*m.states[key] for key in list(m.states.keys())])
-payout_mean = sum([key[2]*m.states[key] for key in list(m.states.keys())])
+        # for s in m.states:
+        #    print(s)
 
-print(slotin_mean)
-print(payout_mean)
-print(payout_mean / slotin_mean)
+        slotin_mean = sum([key[0] * m.states[key] for key in list(m.states.keys())])
+        payout_mean = sum([key[2] * m.states[key] for key in list(m.states.keys())])
+        s_p_rate_mean = sum([key[2] / key[0] * m.states[key] for key in list(m.states.keys())])
 
-import csv
-with open('states-1600-1.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows([[key[1], key[2], m.states[key]] for key in list(m.states.keys())])
+        print(['slot_in', slotin_mean])
+        print(['pay_out', payout_mean])
+        print(['rate', s_p_rate_mean])
+
+        if sys.argv[1] == '1':
+            with open('1-400-states.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows([[key[0], key[2], m.states[key]] for key in list(m.states.keys())])
+        elif sys.argv[1] == '2':
+            with open('2-400-states.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows([[key[0], key[2], m.states[key]] for key in list(m.states.keys())])
+        else:
+            with open('3-400-states.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows([[key[0], key[2], m.states[key]] for key in list(m.states.keys())])
+
+    if i + 1 == 1600:
+        print(len(m.states))
+        print(sum(m.states.values()))
+        timestamp_end = dt.datetime.now()
+        print(timestamp_end - timestamp_begin)
+
+        # for s in m.states:
+        #    print(s)
+
+        slotin_mean = sum([key[0] * m.states[key] for key in list(m.states.keys())])
+        payout_mean = sum([key[2] * m.states[key] for key in list(m.states.keys())])
+        s_p_rate_mean = sum([key[2] / key[0] * m.states[key] for key in list(m.states.keys())])
+
+        print(['slot_in', slotin_mean])
+        print(['pay_out', payout_mean])
+        print(['rate', s_p_rate_mean])
+
+        if sys.argv[1] == '1':
+            with open('1-1600-states.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows([[key[0], key[2], m.states[key]] for key in list(m.states.keys())])
+        elif sys.argv[1] == '2':
+            with open('2-1600-states.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows([[key[0], key[2], m.states[key]] for key in list(m.states.keys())])
+        else:
+            with open('3-1600-states.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerows([[key[0], key[2], m.states[key]] for key in list(m.states.keys())])
